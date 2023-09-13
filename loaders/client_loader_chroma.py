f5d7787fcbacebfeb6ce9044af77c4efcbca86eb
Jeremy <jeremy.artero@gmail.com>
2023-09-13 15:40:26 -0500

    implemented CassandraChatMessageHistory and system message to init the assistant bankflix

--- loaders/client_loader_chroma.py
+++ loaders/client_loader_chroma.py
@@ -3,6 +3,7 @@ import csv
 import openai
 import json
 
+from chromadb.utils import embedding_functions
 from dotenv import dotenv_values
 
 config = dotenv_values('../.env')
@@ -11,7 +12,12 @@ model_id = "text-embedding-ada-002"
 chroma_path = config['CHROMA_PERSISTENT_PATH']
 chroma_client = chromadb.PersistentClient(path=chroma_path)
 
-collection = chroma_client.get_or_create_collection(name="bankflix")
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=openai.api_key,
+                model_name="text-embedding-ada-002"
+            )
+collection = chroma_client.get_or_create_collection(name="bankflix", embedding_function=openai_ef)
+
 
 with open('../resources/clients-dataset.csv', 'r') as file:
     reader = csv.reader(file)
@@ -24,13 +30,10 @@ with open('../resources/clients-dataset.csv', 'r') as file:
         # insert client information and embedding into chroma
         json_data = json.dumps(row_dict)
 
-        # Create embedding for client containing all the rows
-        embedding_client = openai.Embedding.create(input=json_data, model=model_id)['data'][0]['embedding']
-
         # Retrieve CustomerId or any other field if needed
         customer_id = row_dict['CustomerId']
 
         # Insert the row into chroma
-        collection.add(documents=[json_data], ids=[customer_id], embeddings=[embedding_client])
+        collection.add(documents=[json_data], ids=[customer_id])
 
         print(f"Inserted client {customer_id} into ChromaDB")
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import streamlit as st
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
 from dotenv import dotenv_values
 from langchain.agents import initialize_agent, AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+from langchain.memory import CassandraChatMessageHistory
+from langchain.schema import SystemMessage
 
 config = dotenv_values('.env')
 astra_or_chroma = config['ASTRA_OR_CHROMA']
@@ -12,19 +17,42 @@ openai_key = config['OPENAI_API_KEY']
 if astra_or_chroma == "astra":
     from tools.tools_astra import TotalRevenueReaderTool, ClientSimilarityTool, GetClientInformationTool
     tools = [TotalRevenueReaderTool(), ClientSimilarityTool(), GetClientInformationTool()]
-else:
+
+    SECURE_CONNECT_BUNDLE_PATH = config['SECURE_CONNECT_BUNDLE_PATH']
+    ASTRA_CLIENT_ID = config['ASTRA_CLIENT_ID']
+    ASTRA_CLIENT_SECRET = config['ASTRA_CLIENT_SECRET']
+    ASTRA_KEYSPACE_NAME = config['ASTRA_KEYSPACE_NAME']
+
+    # Open a connection to the Astra database
+    cloud_config = {
+        'secure_connect_bundle': SECURE_CONNECT_BUNDLE_PATH
+    }
+    auth_provider = PlainTextAuthProvider(ASTRA_CLIENT_ID, ASTRA_CLIENT_SECRET)
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    astraSession = cluster.connect()
+
+    conversational_memory = CassandraChatMessageHistory(
+        session_id='bankflix-conversation-666',
+        session=astraSession,
+        keyspace=ASTRA_KEYSPACE_NAME,
+        ttl_seconds=3600,
+    )
+    conversational_memory.clear()
+else: # chroma
     from tools.tools_chroma import ClientSimilarityTool
     tools = [ClientSimilarityTool()]
+    conversational_memory = ConversationBufferWindowMemory(
+        memory_key='chat_history',
+        ai_prefix='AI',
+        k=5,
+        return_messages=True
+    )
 
+system_message = SystemMessage(content="You are Bankflix, a sophisticated bank assistant, specialized in credit scores and "
+                                       "currency transactions. With expert knowledge and precision, you are here to "
+                                       "provide accurate information and solutions to banking queries.")
 
 ### Initialize the LangChain Agent #########
-conversational_memory = ConversationBufferWindowMemory(
-    memory_key='chat_history',
-    ai_prefix='AI',
-    k=5,
-    return_messages=True
-)
-
 llm = ChatOpenAI(
     openai_api_key=openai_key,
     temperature=0,
@@ -35,11 +63,14 @@ agent = initialize_agent(
     agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
     tools=tools,
     llm=llm,
-    max_iterations=5,
-    verbose=True,
-    memory=conversational_memory,
+    max_iterations=3,
+    verbose=False,
+    chat_memory=conversational_memory,
     handle_parsing_errors=True,
-    early_stopping_method='generate'
+    early_stopping_method='generate',
+    agent_kwargs={
+        "system_message": system_message.content
+    }
 )
 
 # set title
@@ -49,9 +80,11 @@ st.title('🏦 BankFlix Chatbot')
 st.header("Welcome dear bank employee!")
 
 user_question = st.text_input('Ask a question here:')
+
 # if the question has more than 5 characters, run the agent
 if len(user_question) > 5:
     with st.spinner(text="In progress..."):
-        response = agent.run('{}, {}'.format(user_question, user_question))
+        conversational_memory.add_user_message(user_question)
+        response = agent.run(input=user_question, chat_history=conversational_memory.messages)
         st.write(response)
 
